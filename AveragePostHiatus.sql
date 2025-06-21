f49e735488a980e8a5e5445a87a17e59a426039c
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-06-22 00:03:20 +0300

    Update AveragePostHiatus.sql

--- AveragePostHiatus.sql
+++ AveragePostHiatus.sql
@@ -27,3 +27,6 @@ LEFT JOIN tb2
 ON tb2.user_id = tb1.user_id
 WHERE days_between >0; 
 
+-- Given a table of Facebook posts, for each user who posted at least twice in 2021, 
+-- write a query to find the number of days between each user’s first post of the year and last post of the year in the year 2021. 
+-- Output the user and number of the days between each user's first and last post.

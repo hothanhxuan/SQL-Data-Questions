6c8ad96cc077fce99b984da071518fca5b29a383
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-07-12 02:18:50 +0700

    Update ActiveUserRetention.sql

--- DataLemur/HardLevel/ActiveUserRetention.sql
+++ DataLemur/HardLevel/ActiveUserRetention.sql
@@ -1,14 +1,22 @@
 SELECT 
-  EXTRACT(MONTH FROM curr_month.event_date) AS mth, 
-  COUNT(DISTINCT curr_month.user_id) AS monthly_active_users 
-FROM user_actions AS curr_month
-WHERE EXISTS (
-  SELECT last_month.user_id 
-  FROM user_actions AS last_month
-  WHERE last_month.user_id = curr_month.user_id
-    AND EXTRACT(MONTH FROM last_month.event_date) =
-    EXTRACT(MONTH FROM curr_month.event_date - interval '1 month')
-)
-  AND EXTRACT(MONTH FROM curr_month.event_date) = 7
-  AND EXTRACT(YEAR FROM curr_month.event_date) = 2022
-GROUP BY EXTRACT(MONTH FROM curr_month.event_date);
+      EXTRACT(MONTH FROM current.event_date) AS month 
+      ,COUNT(DISTINCT current.user_id) AS monthly_active_users 
+FROM user_actions AS current 
+WHERE EXISTS 
+            (
+            SELECT 
+                  last.user_id 
+            FROM user_actions AS last 
+            WHERE last.user_id = current.user_id 
+              AND EXTRACT(MONTH FROM last.event_date) 
+                  = EXTRACT(MONTH FROM current.event_date - interval '1 month')
+            )
+AND EXTRACT(MONTH FROM current.event_date) = 7
+AND EXTRACT(YEAR FROM current.event_date) = 2022
+GROUP BY EXTRACT(MONTH FROM current.event_date);
+
+-- Assume you're given a table containing information on Facebook user actions. 
+-- Write a query to obtain number of monthly active users (MAUs) in July 2022, including the month in numerical format "1, 2, 3".
+
+-- Hint:
+-- An active user is defined as a user who has performed actions such as 'sign-in', 'like', or 'comment' in both the current month and the previous month.

71ffb7003e31c461f10dcc4f3d3fabafe1b68956
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-07-15 14:20:21 +0700

    Update MaximizePrimeItemInventory.sql

--- DataLemur/HardLevel/MaximizePrimeItemInventory.sql
+++ DataLemur/HardLevel/MaximizePrimeItemInventory.sql
@@ -1,39 +1,31 @@
 WITH count_item_and_total_sqr AS
 (
   SELECT 
-        item_type
-        ,SUM(square_footage) FILTER (WHERE item_type = 'prime_eligible')AS total_sqr_prime
-        ,COUNT(*) (WHERE item_type = 'non_prime') AS prime_item_count 
-        ,SUM(square_footage) FILTER (WHERE item_type = 'prime_eligible')AS total_sqr_non_prime
-        ,COUNT(*) (WHERE item_type = 'non_prime') AS non_prime_item_count 
+        SUM(square_footage) FILTER (WHERE item_type = 'prime_eligible')AS total_sqr_prime
+        ,COUNT(item_id) FILTER (WHERE item_type = 'prime_eligible') AS batch_prime_count 
+        ,SUM(square_footage) FILTER (WHERE item_type = 'not_prime')AS total_sqr_non_prime
+        ,COUNT(item_id) FILTER (WHERE item_type = 'not_prime') AS batch_non_prime_count 
   FROM inventory
-  GROUP BY item_type
 )
-,
-calculation_for_prime AS
+max_prime_cnt AS
 (
   SELECT
-        item_type
-        ,total_sqr_prime
-        ,item_count 
-        ,FLOOR(500000/total_sqr_prime) AS prime_item_combination_count -- warehouse area / total prime area 
-        ,FLOOR(500000/total_sqr_prime) * item_count AS prime_item_count -- (warehouse area / total prime area)*Number of Prime Batches
-  FROM count_item_and_total_sqr 
-  WHERE item_type = 'prime_eligible'
-)
-,
-calculation_for_non_prime AS
-(
-  SELECT
-        item_type
-        ,total_sqr_prime
-        ,FLOOR(500000 - (total_sqr_prime * prime_item_combination_count))  AS non_prime_item_combination_count -- (total areas - remaining_space_for_non)  
-        ,FLOOR((500000 - (total_sqr_prime * prime_item_combination_count)) * item_count) AS non_prime_item_count -- (warehouse area / total prime area)*Number of Prime Batches
-  FROM calculation_for_prime 
-  WHERE item_type = 'non_prime'
+          FLOOR(500000/total_sqr_prime) AS prime_occupied_area -- warehouse area / total prime area 
+          ,FLOOR(500000/total_sqr_prime) * batch_prime_count AS prime_item_count -- (warehouse area / total prime area)*Number of Prime Batches
+  FROM count_item_and_total_sqr
 )
 
-500000 - (total_sqr_prime * prime_item_combination_count) / total_sqr_prime ) * item_count AS non_prime_item_count
+SELECT 
+      'prime_eligible' AS item_type 
+      ,prime_item_count
+FROM max_prime_cnt 
+
+UNION 
+
+SELECT
+      'not_prime' AS item_type  
+      ,FLOOR((500000 - prime_occupied_area)*batch_non_prime_count) AS non_prime_occupied_area -- (total areas - remaining_space_for_non)  
+FROM max_prime_cnt, count_item_and_total_sqr;   
 
 
 -- Amazon wants to maximize the storage capacity of its 500,000 square-foot warehouse by prioritizing a specific batch of prime items. 

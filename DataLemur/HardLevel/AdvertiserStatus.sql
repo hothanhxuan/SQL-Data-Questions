aa26d6b5ac85c67769cf1f942585a22e34702149
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-07-21 15:03:11 +0700

    Update AdvertiserStatus.sql

--- DataLemur/HardLevel/AdvertiserStatus.sql
+++ DataLemur/HardLevel/AdvertiserStatus.sql
@@ -1,18 +1,19 @@
 WITH raw_data AS
 (
 SELECT 
-      tb1.user_id,
-      tb1.status AS current_status,
-      CASE WHEN tb2.paid IS NULL THEN 'not paid' ELSE 'paid' END AS Payment_on_Day_T 
+      COALESCE (tb1.user_id, tb2.user_id) AS user_id
+      ,tb1.status AS current_status 
+      ,CASE WHEN tb2.paid IS NULL THEN 'not paid' ELSE 'paid' END AS Payment_on_Day_T
 FROM advertiser AS tb1
-LEFT JOIN daily_pay AS tb2
+FULL OUTER JOIN daily_pay AS tb2
 ON tb1.user_id = tb2.user_id
 )
 SELECT 
       user_id,
       CASE WHEN payment_on_day_t = 'not paid' THEN 'CHURN'
-           WHEN payment_on_day_t = 'paid' AND current_status IN ('NEW', 'EXISTING', 'RESURRECT') THEN 'EXISTING'
-           WHEN payment_on_day_t = 'paid' AND current_status = 'CHURN' THEN 'RESURRECT' 
+          WHEN payment_on_day_t = 'paid' AND current_status IN ('NEW', 'EXISTING', 'RESURRECT') THEN 'EXISTING'
+          WHEN payment_on_day_t = 'paid' AND current_status = 'CHURN' THEN 'RESURRECT'
+          WHEN payment_on_day_t = 'paid' AND current_status IS NULL THEN 'NEW'
       END AS new_status 
 FROM raw_data
 ORDER BY user_id; 

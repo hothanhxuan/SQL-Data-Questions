fb8a8ba46fb5d0d757e97dc413a95d38f62919b2
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-07-22 23:31:34 +0700

    Update 3-ToppingPizzas.sql

--- DataLemur/HardLevel/3-ToppingPizzas.sql
+++ DataLemur/HardLevel/3-ToppingPizzas.sql
@@ -3,3 +3,15 @@ INNER JOIN pizza_toppings AS tb2
 ON tb1.topping_name < tb2.topping_name
 INNER JOIN pizza_toppings AS tb3
 ON tb2.topping_name < tb3.topping_name;
+
+
+-- You’re a consultant for a major pizza chain that will be running a promotion where all 3-topping pizzas will be sold for a fixed price, and are trying to understand the costs involved.
+-- Given a list of pizza toppings, consider all the possible 3-topping pizzas, and print out the total cost of those 3 toppings. 
+-- Sort the results with the highest total cost on the top followed by pizza toppings in ascending order.
+
+-- Break ties by listing the ingredients in alphabetical order, starting from the first ingredient, followed by the second and third.
+-- P.S. Be careful with the spacing (or lack of) between each ingredient. Refer to our Example Output.
+
+-- Notes:
+-- Do not display pizzas where a topping is repeated. For example, ‘Pepperoni,Pepperoni,Onion Pizza’.
+-- Ingredients must be listed in alphabetical order. For example, 'Chicken,Onions,Sausage'. 'Onion,Sausage,Chicken' is not acceptable.

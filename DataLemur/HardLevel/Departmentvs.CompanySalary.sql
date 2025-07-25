3ad9f6dc1ec7fdfe3e0ee680813f8ffaf903f169
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-07-26 00:02:18 +0700

    Update Departmentvs.CompanySalary.sql

--- DataLemur/HardLevel/Departmentvs.CompanySalary.sql
+++ DataLemur/HardLevel/Departmentvs.CompanySalary.sql
@@ -3,3 +3,11 @@ SELECT
 FROM salary
 GROUP BY payment_date
 HAVING EXTRACT(MONTH FROM payment_date) = 03;
+
+
+-- You work as a data analyst for a FAANG company that tracks employee salaries over time. 
+-- The company wants to understand how the average salary in each department compares to the company's overall average salary each month.
+
+-- Write a query to compare the average salary of employees in each department to the company's average salary for March 2024. 
+-- Return the comparison result as 'higher', 'lower', or 'same' for each department. 
+-- Display the department ID, payment month (in MM-YYYY format), and the comparison result.

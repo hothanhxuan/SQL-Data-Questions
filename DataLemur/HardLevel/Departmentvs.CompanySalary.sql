652f70d17feeb5dc01a2bb8bdabda29c37330a8b
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-07-27 00:20:37 +0700

    Update Departmentvs.CompanySalary.sql

--- DataLemur/HardLevel/Departmentvs.CompanySalary.sql
+++ DataLemur/HardLevel/Departmentvs.CompanySalary.sql
@@ -1,8 +1,36 @@
+WITH company_avg AS
+(
 SELECT
-      ROUND(AVG(amount), 1) AS avg_salary_company_March2024
+      TO_CHAR(payment_date, 'MM-YYYY') AS payment_date
+      ,ROUND(AVG(amount), 1) AS avg_salary_company_March2024
 FROM salary
 GROUP BY payment_date
-HAVING EXTRACT(MONTH FROM payment_date) = 03;
+HAVING EXTRACT(MONTH FROM payment_date) = 03
+)
+,
+department_avg AS
+(
+SELECT 
+      e.department_id
+      ,TO_CHAR(s.payment_date, 'MM-YYYY') AS payment_date
+      ,ROUND(AVG(s.amount),1) AS avg_by_department
+FROM salary AS s
+INNER JOIN employee AS e
+ON s.employee_id = e.employee_id
+GROUP BY department_id, s.payment_date
+HAVING EXTRACT(MONTH FROM payment_date) = 03
+)
+SELECT 
+      d.department_id
+      ,d.payment_date
+      ,CASE 
+          WHEN d.avg_by_department > c.avg_salary_company_March2024 THEN 'higher'
+          WHEN d.avg_by_department < c.avg_salary_company_March2024 THEN 'lower'
+          ELSE 'same'
+        END AS comparison 
+FROM department_avg AS d
+INNER JOIN company_avg AS c
+ON d.payment_date = c.payment_date;
 
 
 -- You work as a data analyst for a FAANG company that tracks employee salaries over time. 

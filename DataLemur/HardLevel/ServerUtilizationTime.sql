3d39f061e1fd93dda49b2ef725696503513fb5f7
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-07-30 00:25:14 +0700

    Create ServerUtilizationTime.sql

---
+++ DataLemur/HardLevel/ServerUtilizationTime.sql
@@ -0,0 +1,8 @@
+
+
+-- Amazon Web Services (AWS) is powered by fleets of servers. Senior management has requested data-driven solutions to optimize server usage.
+-- Write a query that calculates the total time that the fleet of servers was running. The output should be in units of full days.
+
+-- Assumptions:
+-- Each server might start and stop several times.
+-- The total time in which the server fleet is running can be calculated as the sum of each server's uptime.

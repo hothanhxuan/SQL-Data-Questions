9ef1802ce7b26ffe3228563a6128037d6542bbd9
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-08-07 01:25:09 +0700

    Update NewCompanies.sql

--- HackerRank/Medium/NewCompanies.sql
+++ HackerRank/Medium/NewCompanies.sql
@@ -1,1 +1,6 @@
-
+WITH tb1 AS
+    (SELECT C.company_code, C.founder, L.lead_manager_code
+    FROM Company AS C
+    LEFT JOIN Lead_Manager AS L
+    ON C.company_code = L.company_code 
+    ORDER BY C.company_code) 

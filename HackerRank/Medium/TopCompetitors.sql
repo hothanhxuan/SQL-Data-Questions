697d39b89b75e0184124f0011b96c4092b3f1604
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-08-10 02:09:38 +0700

    Update TopCompetitors.sql

--- HackerRank/Medium/TopCompetitors.sql
+++ HackerRank/Medium/TopCompetitors.sql
@@ -1,4 +1,21 @@
-
+SELECT H.hacker_id, H.name
+  
+FROM Hackers AS H
+LEFT JOIN Submissions AS S
+ON H.hacker_id = S.hacker_id
+LEFT JOIN Challenges AS C
+ON S.challenge_id = C.challenge_id 
+LEFT JOIN Difficulty AS D
+ON C.difficulty_level = D.difficulty_level
+  
+WHERE 
+    S.score = D.score
+GROUP BY 
+    H.hacker_id, H.name
+HAVING 
+    COUNT(S.submission_id) > 1
+ORDER BY
+    COUNT(S.submission_id) DESC, H.hacker_id ASC; 
 
 -- Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
 -- Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. 

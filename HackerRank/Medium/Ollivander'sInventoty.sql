60f9d97612e31300b7f45157e0c1cc73b88b0a30
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-08-12 19:06:11 +0700

    Update Ollivander'sInventoty.sql

--- HackerRank/Medium/Ollivander'sInventoty.sql
+++ HackerRank/Medium/Ollivander'sInventoty.sql
@@ -1,4 +1,35 @@
+SELECT t.id, t.age, t.coins_needed, t.power
+FROM (SELECT 
+            W.id
+            ,WP.age
+            ,W.coins_needed
+            ,W.power 
+        FROM Wands_Property AS WP
+        LEFT JOIN Wands AS W
+        ON WP.code = W.code
+        where is_evil = 0 
+        GROUP BY WP.age, W.power, W.id, W.coins_needed) AS t
 
+JOIN 
+
+(SELECT age, MIN(coins_needed) AS min_coin, power
+FROM (SELECT 
+            W.id
+            ,WP.age
+            ,W.coins_needed
+            ,W.power 
+        FROM Wands_Property AS WP
+        LEFT JOIN Wands AS W
+        ON WP.code = W.code
+        where is_evil = 0 
+        GROUP BY WP.age, W.power, W.id, W.coins_needed) AS t
+GROUP BY power, age
+) AS m
+
+ON t.age = m.age
+AND t.power = m.power
+AND t.coins_needed = m.min_coin
+ORDER BY t.power DESC, t.age DESC; 
 
 
 -- Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.

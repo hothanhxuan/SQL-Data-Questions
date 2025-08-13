2b7fba1fbe8c34a1a6f8b2e3fc8a79c9038dc05a
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-08-14 00:38:45 +0700

    Update SymmetricPairs.sql

--- HackerRank/Medium/SymmetricPairs.sql
+++ HackerRank/Medium/SymmetricPairs.sql
@@ -1,4 +1,12 @@
-
+SELECT DISTINCT f1.x AS x1, f1.y AS y1 
+FROM functions f1 
+JOIN functions f2 
+ON f1.x = f2.y 
+    AND f1.y = f2.x 
+WHERE f1.x <= f1.y 
+group by f1.x,f1.y 
+having (count(x1)>1 and f1.x=f1.y) or (f1.x<>f1.y) 
+ORDER BY f1.x
 
 -- You are given a table, Functions, containing two columns: X and Y.
 -- Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1.

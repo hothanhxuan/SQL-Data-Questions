6f7891a2cd9761776be686805a747aff62f9eab4
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-08-14 01:02:54 +0700

    Update SymmetricPairs.sql

--- HackerRank/Medium/SymmetricPairs.sql
+++ HackerRank/Medium/SymmetricPairs.sql
@@ -1,12 +1,13 @@
-SELECT DISTINCT f1.x AS x1, f1.y AS y1 
+SELECT DISTINCT f1.X AS X1, f1.Y AS Y1 
 FROM functions f1 
 JOIN functions f2 
-ON f1.x = f2.y 
-    AND f1.y = f2.x 
-WHERE f1.x <= f1.y 
-group by f1.x,f1.y 
-having (count(x1)>1 and f1.x=f1.y) or (f1.x<>f1.y) 
-ORDER BY f1.x
+ON f1.X = f2.Y -- CẶP (X1, Y2) 
+    AND f1.Y = f2.X -- CẶP (X2, Y1)
+WHERE f1.X <= f1.Y  -- Cặp dạng (a,b) và (b,a) với a ≠ b 
+GROUP BY f1.X, f1.Y 
+HAVING (COUNT(X1)>1 and f1.X = f1.Y) or (f1.X <> f1.Y) -- Cặp dạng (a,a) -> Để in ra (a,a) thì bảng phải có ít nhất 2 bản ghi (a,a) 
+    -- nếu chỉ có 1 hàng (a,a) thì không được coi là “symmetric pair” 
+ORDER BY f1.X
 
 -- You are given a table, Functions, containing two columns: X and Y.
 -- Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1.

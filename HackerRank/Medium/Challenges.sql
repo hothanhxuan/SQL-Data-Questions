8db7b8195cea004b3967de8d258e1253654e2c5c
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-08-17 23:07:40 +0700

    Update Challenges.sql

--- HackerRank/Medium/Challenges.sql
+++ HackerRank/Medium/Challenges.sql
@@ -1,3 +1,34 @@
+SELECT HC.hacker_id, HC.name, HC.cnt
+FROM (
+    SELECT H.hacker_id, H.name, COUNT(C.challenge_id) AS cnt
+    FROM Hackers AS H
+    LEFT JOIN Challenges AS C
+        ON H.hacker_id = C.hacker_id
+    GROUP BY H.hacker_id, H.name
+) AS HC
+JOIN (
+    SELECT cnt, COUNT(*) AS cnt_count
+    FROM (
+        SELECT H.hacker_id, H.name, COUNT(C.challenge_id) AS cnt
+        FROM Hackers AS H
+        LEFT JOIN Challenges AS C
+            ON H.hacker_id = C.hacker_id
+        GROUP BY H.hacker_id, H.name
+    ) AS T
+    GROUP BY cnt
+) AS CG
+ON HC.cnt = CG.cnt
+WHERE 
+    CG.cnt_count = 1
+    OR HC.cnt = (SELECT MAX(cnt)
+                 FROM (
+                     SELECT COUNT(C.challenge_id) AS cnt
+                     FROM Hackers AS H
+                     LEFT JOIN Challenges AS C
+                         ON H.hacker_id = C.hacker_id
+                     GROUP BY H.hacker_id, H.name
+                 ) AS M)
+ORDER BY HC.cnt DESC, HC.hacker_id;
 
 -- Julia asked her students to create some coding challenges. 
 -- Write a query to print the hacker_id, name, and the total number of challenges created by each student. 

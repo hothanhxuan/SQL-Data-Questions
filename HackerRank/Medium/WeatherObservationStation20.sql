7a2fd9fe4e5ccc2fd797e936299276b8d08af20a
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-08-22 18:40:25 +0700

    Update WeatherObservationStation20.sql

--- HackerRank/Medium/WeatherObservationStation20.sql
+++ HackerRank/Medium/WeatherObservationStation20.sql
@@ -1,4 +1,9 @@
 
+  SELECT LAT_N AS val
+  FROM STATION
+  WHERE (
+      SELECT COUNT(*) FROM STATION t2 WHERE t2.LAT_N < t1.LAT_N) 
+     
 
 A median is defined as a number separating the higher half of a data set from the lower half. 
 Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.

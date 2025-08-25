b99d562d563c834ebbb2a931694a79f25e6b0097
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-08-25 23:36:01 +0700

    Update WeatherObservationStation20.sql

--- HackerRank/Medium/WeatherObservationStation20.sql
+++ HackerRank/Medium/WeatherObservationStation20.sql
@@ -10,5 +10,5 @@ JOIN (SELECT COUNT(*) AS cnt FROM STATION WHERE LAT_N IS NOT NULL) AS c
 WHERE rn IN ((cnt + 1) DIV 2, (cnt + 2) DIV 2);
      
 
-A median is defined as a number separating the higher half of a data set from the lower half. 
-Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.
+-- A median is defined as a number separating the higher half of a data set from the lower half. 
+-- Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.

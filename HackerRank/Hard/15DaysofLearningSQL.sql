3ec949fc8182cd6c2094e4131ea4548198843976
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-09-01 00:02:17 +0700

    Create 15DaysofLearningSQL.sql

---
+++ HackerRank/Hard/15DaysofLearningSQL.sql
@@ -0,0 +1,13 @@
+-- Julia conducted a  days of learning SQL contest. The start date of the contest was March 01, 2016 and the end date was March 15, 2016.
+-- Write a query to print total number of unique hackers who made at least  submission each day (starting on the first day of the contest), 
+-- and find the hacker_id and name of the hacker who made maximum number of submissions each day. 
+-- If more than one such hacker has a maximum number of submissions, print the lowest hacker_id. 
+-- The query should print this information for each day of the contest, sorted by the date.
+
+-- Sample Output
+-- 2016-03-01 4 20703 Angela
+-- 2016-03-02 2 79722 Michael
+-- 2016-03-03 2 20703 Angela
+-- 2016-03-04 2 20703 Angela
+-- 2016-03-05 1 36396 Frank
+-- 2016-03-06 1 20703 Angela

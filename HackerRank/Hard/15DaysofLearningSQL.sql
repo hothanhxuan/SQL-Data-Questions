9a744ef7297263a0fcbaef8788b1c9efb5042016
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-09-02 23:49:34 +0700

    Update 15DaysofLearningSQL.sql

--- HackerRank/Hard/15DaysofLearningSQL.sql
+++ HackerRank/Hard/15DaysofLearningSQL.sql
@@ -1,3 +1,7 @@
+
+
+
+
 -- Julia conducted a  days of learning SQL contest. The start date of the contest was March 01, 2016 and the end date was March 15, 2016.
 -- Write a query to print total number of unique hackers who made at least  submission each day (starting on the first day of the contest), 
 -- and find the hacker_id and name of the hacker who made maximum number of submissions each day. 

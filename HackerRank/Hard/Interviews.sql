e223ddb35e1288b586d079d39497657fd0c2ce71
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-09-12 00:01:52 +0700

    Update Interviews.sql

--- HackerRank/Hard/Interviews.sql
+++ HackerRank/Hard/Interviews.sql
@@ -5,8 +5,6 @@ SELECT H.hacker_id, H.name, COUNT(C.challenge_id) AS cnt
     GROUP BY H.hacker_id, H.name
 
 
-
-
 -- Samantha interviews many candidates from different colleges using coding challenges and contests. 
 -- Write a query to print the contest_id, hacker_id, name, and the sums of total_submissions, total_accepted_submissions, 
 -- total_views, and total_unique_views for each contest sorted by contest_id. Exclude the contest from the result if all four sums are .

8296a32d6de008e448b593e762770664ca66d66d
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-09-19 00:25:32 +0700

    Update EmployeesEarningMoreThanTheirManager.sql

--- LeetCode/EmployeesEarningMoreThanTheirManager.sql
+++ LeetCode/EmployeesEarningMoreThanTheirManager.sql
@@ -4,18 +4,18 @@ JOIN Employee M
   ON E.managerId = M.id
 WHERE E.salary > M.salary;
 
-Write a solution to find the employees who earn more than their managers.
-Return the result table in any order.
-The result format is in the following example.
+-- Write a solution to find the employees who earn more than their managers.
+-- Return the result table in any order.
+-- The result format is in the following example.
 
-| id | name  | salary | managerId |
-| -- | ----- | ------ | --------- |
-| 1  | Joe   | 70000  | 3         |
-| 2  | Henry | 80000  | 4         |
-| 3  | Sam   | 60000  | null      |
-| 4  | Max   | 90000  | null      |
+-- | id | name  | salary | managerId |
+-- | -- | ----- | ------ | --------- |
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | null      |
+-- | 4  | Max   | 90000  | null      |
 
-Output:
-| Employee |
-| -------- |
-| Joe      |
+-- Output:
+-- | Employee |
+-- | -------- |
+-- | Joe      |

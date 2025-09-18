d8704494a57e0e2b1330f581b43b0262ed8f174d
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-09-19 00:25:15 +0700

    Create DuplicateEmails.sql

---
+++ LeetCode/DuplicateEmails.sql
@@ -0,0 +1,24 @@
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(*) > 1;
+
+-- Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+-- Return the result table in any order.
+-- The result format is in the following example
+
+-- Input: 
+-- Person table:
+-- +----+---------+
+-- | id | email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- Output: 
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+

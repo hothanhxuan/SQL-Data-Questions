44e03c5f39f596e3a2dedea8fd686409fd5f733e
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-09-20 00:33:42 +0700

    Create CustomersWhoNeverOrder.sql

---
+++ LeetCode/CustomersWhoNeverOrder.sql
@@ -0,0 +1,34 @@
+SELECT C.name AS Customers
+FROM Customers AS C
+LEFT JOIN Orders AS O
+ON C.id = O.customerID
+WHERE O.customerID IS NULL;
+
+-- Write a solution to find all customers who never order anything.
+-- Return the result table in any order.
+-- The result format is in the following example.
+
+-- Input: 
+-- Customers table:
+-- +----+-------+
+-- | id | name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Orders table:
+-- +----+------------+
+-- | id | customerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- Output: 
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+

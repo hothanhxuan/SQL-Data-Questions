1b7ba279bbe69a0481a320a663903f53aea30745
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-09-21 23:06:03 +0700

    Create DeleteDuplicateEmails.sql

---
+++ LeetCode/DeleteDuplicateEmails.sql
@@ -0,0 +1,25 @@
+DELETE p1.*
+FROM Person p1
+JOIN Person p2
+  ON p1.email = p2.email
+ AND p1.id > p2.id;
+
+-- Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+-- For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+
+-- Input: 
+-- Person table:
+-- +----+------------------+
+-- | id | email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- | 3  | john@example.com |
+-- +----+------------------+
+-- Output: 
+-- +----+------------------+
+-- | id | email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- +----+------------------+

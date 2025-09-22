103d4a6af36a6afc7455c93a7ea13db1a7781fab
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-09-22 22:50:21 +0700

    Create RisingTemperature.sql

---
+++ LeetCode/RisingTemperature.sql
@@ -0,0 +1,22 @@
+
+ 
+
+-- Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+-- Return the result table in any order.
+-- Input: 
+-- Weather table:
+-- +----+------------+-------------+
+-- | id | recordDate | temperature |
+-- +----+------------+-------------+
+-- | 1  | 2015-01-01 | 10          |
+-- | 2  | 2015-01-02 | 25          |
+-- | 3  | 2015-01-03 | 20          |
+-- | 4  | 2015-01-04 | 30          |
+-- +----+------------+-------------+
+-- Output: 
+-- +----+
+-- | id |
+-- +----+
+-- | 2  |
+-- | 4  |
+-- +----+

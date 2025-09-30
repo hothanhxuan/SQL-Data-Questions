2fe45ba4df22d63a1bb089f3f416d5ca8546ab92
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-09-30 20:56:33 +0700

    Update EmployeeBonus.sql

--- LeetCode/EmployeeBonus.sql
+++ LeetCode/EmployeeBonus.sql
@@ -1,1 +1,28 @@
+Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+Return the result table in any order.
 
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+

d55305565602ba041096bb064eb8ea2ce6bed8ce
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-10-06 23:19:39 +0300

    Create FindCustomerReferee.sql

---
+++ LeetCode/FindCustomerReferee.sql
@@ -0,0 +1,27 @@
+Find the names of the customer that are either:
+
+referred by any customer with id != 2.
+not referred by any customer.
+Return the result table in any order.
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+

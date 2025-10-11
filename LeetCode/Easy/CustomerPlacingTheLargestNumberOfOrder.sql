7e59831d1944c924918e08ff314c0fef56784e01
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-10-11 22:50:38 +0300

    Update CustomerPlacingTheLargestNumberOfOrder.sql

--- LeetCode/Easy/CustomerPlacingTheLargestNumberOfOrder.sql
+++ LeetCode/Easy/CustomerPlacingTheLargestNumberOfOrder.sql
@@ -1,9 +1,8 @@
 SELECT customer_number
-FROM
-    (SELECT *,
-        ROW_NUMBER() OVER (PARTITION BY customer_number) AS rnk
-    FROM Orders) AS cnt
-WHERE rnk = 2;
+FROM orders
+GROUP BY customer_number
+ORDER BY COUNT(order_number) DESC
+LIMIT 1;
 
 -- Write a solution to find the customer_number for the customer who has placed the largest number of orders.
 -- The test cases are generated so that exactly one customer will have placed more orders than any other customer.

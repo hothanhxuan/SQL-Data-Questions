8e52548f0712093d1648e096607c40937796ed41
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-10-13 23:22:28 +0300

    Update ClassesWithatLeast5Students.sql

--- LeetCode/Easy/ClassesWithatLeast5Students.sql
+++ LeetCode/Easy/ClassesWithatLeast5Students.sql
@@ -1,29 +1,34 @@
-Write a solution to find all the classes that have at least five students.
-Return the result table in any order.
+SELECT class
+FROM Courses 
+GROUP BY class
+HAVING COUNT(student) >=5; 
 
-Input: 
-Courses table:
-+---------+----------+
-| student | class    |
-+---------+----------+
-| A       | Math     |
-| B       | English  |
-| C       | Math     |
-| D       | Biology  |
-| E       | Math     |
-| F       | Computer |
-| G       | Math     |
-| H       | Math     |
-| I       | Math     |
-+---------+----------+
-Output: 
-+---------+
-| class   |
-+---------+
-| Math    |
-+---------+
-Explanation: 
-- Math has 6 students, so we include it.
-- English has 1 student, so we do not include it.
-- Biology has 1 student, so we do not include it.
-- Computer has 1 student, so we do not include it.
+-- Write a solution to find all the classes that have at least five students.
+-- Return the result table in any order.
+
+-- Input: 
+-- Courses table:
+-- +---------+----------+
+-- | student | class    |
+-- +---------+----------+
+-- | A       | Math     |
+-- | B       | English  |
+-- | C       | Math     |
+-- | D       | Biology  |
+-- | E       | Math     |
+-- | F       | Computer |
+-- | G       | Math     |
+-- | H       | Math     |
+-- | I       | Math     |
+-- +---------+----------+
+-- Output: 
+-- +---------+
+-- | class   |
+-- +---------+
+-- | Math    |
+-- +---------+
+-- Explanation: 
+-- - Math has 6 students, so we include it.
+-- - English has 1 student, so we do not include it.
+-- - Biology has 1 student, so we do not include it.
+-- - Computer has 1 student, so we do not include it.

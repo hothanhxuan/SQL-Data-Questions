68df1a262f343d190436ba87f8019504626fc88b
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-10-24 02:54:06 +0700

    Update TriangleJudgement.sql

--- LeetCode/Easy/TriangleJudgement.sql
+++ LeetCode/Easy/TriangleJudgement.sql
@@ -1,20 +1,27 @@
+SELECT x, y, z,
+  CASE
+    WHEN x + y > z AND y + z > x AND x + z > y
+    THEN 'Yes'
+    ELSE 'No'
+  END AS triangle
+FROM Triangle;
+  
 
+-- Report for every three line segments whether they can form a triangle.
+-- Return the result table in any order.
 
-Report for every three line segments whether they can form a triangle.
-Return the result table in any order.
-
-Input: 
-Triangle table:
-+----+----+----+
-| x  | y  | z  |
-+----+----+----+
-| 13 | 15 | 30 |
-| 10 | 20 | 15 |
-+----+----+----+
-Output: 
-+----+----+----+----------+
-| x  | y  | z  | triangle |
-+----+----+----+----------+
-| 13 | 15 | 30 | No       |
-| 10 | 20 | 15 | Yes      |
-+----+----+----+----------+
+-- Input: 
+-- Triangle table:
+-- +----+----+----+
+-- | x  | y  | z  |
+-- +----+----+----+
+-- | 13 | 15 | 30 |
+-- | 10 | 20 | 15 |
+-- +----+----+----+
+-- Output: 
+-- +----+----+----+----------+
+-- | x  | y  | z  | triangle |
+-- +----+----+----+----------+
+-- | 13 | 15 | 30 | No       |
+-- | 10 | 20 | 15 | Yes      |
+-- +----+----+----+----------+

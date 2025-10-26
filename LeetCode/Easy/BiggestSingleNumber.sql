cb280b2e27e9ad2f616840ab9f37e889feb34625
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-10-26 22:15:18 +0200

    Update BiggestSingleNumber.sql

--- LeetCode/Easy/BiggestSingleNumber.sql
+++ LeetCode/Easy/BiggestSingleNumber.sql
@@ -1,3 +1,9 @@
+SELECT MAX(num) AS num 
+FROM(SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1) AS single_num;
+
 -- A single number is a number that appeared only once in the MyNumbers table.
 -- Find the largest single number. If there is no single number, report null.
 

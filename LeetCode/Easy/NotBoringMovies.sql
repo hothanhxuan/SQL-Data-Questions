cb4db7b0eaec42c8283431ea8a190d88e2ae2fb9
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-10-30 01:21:04 +0200

    Update NotBoringMovies.sql

--- LeetCode/Easy/NotBoringMovies.sql
+++ LeetCode/Easy/NotBoringMovies.sql
@@ -1,4 +1,7 @@
-
+SELECT id, movie, description, rating 
+FROM Cinema
+WHERE id % 2 <> 0 AND description <> 'boring'
+ORDER BY rating DESC;
 
 -- Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
 -- Return the result table ordered by rating in descending order.

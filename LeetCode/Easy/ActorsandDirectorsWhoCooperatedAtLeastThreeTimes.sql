aa88d61f74ed077eaa0f6a56db833efaecc616d2
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-11-03 16:27:57 +0200

    Update ActorsandDirectorsWhoCooperatedAtLeastThreeTimes.sql

--- LeetCode/Easy/ActorsandDirectorsWhoCooperatedAtLeastThreeTimes.sql
+++ LeetCode/Easy/ActorsandDirectorsWhoCooperatedAtLeastThreeTimes.sql
@@ -1,3 +1,7 @@
+SELECT actor_id, director_id
+FROM ActorDirector 
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >=3; 
 
 -- Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
 -- Return the result table in any order.

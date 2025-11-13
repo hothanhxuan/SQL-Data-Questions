47fa11739d28dbc20a561e26f56f41666b357987
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-11-13 19:16:09 +0200

    Update ArticleViewsI.sql

--- LeetCode/Easy/ArticleViewsI.sql
+++ LeetCode/Easy/ArticleViewsI.sql
@@ -1,4 +1,7 @@
-
+SELECT DISTINCT author_id AS id
+FROM Views 
+WHERE author_id = viewer_id 
+ORDER BY author_id;
 
 -- Write a solution to find all the authors that viewed at least one of their own articles.
 -- Return the result table sorted by id in ascending order.

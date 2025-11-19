16761dc5738efd45c0fb87191ef5846e93fa879f
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-11-19 20:37:06 +0200

    Update MarketAnalysisI.sql

--- LeetCode/Easy/MarketAnalysisI.sql
+++ LeetCode/Easy/MarketAnalysisI.sql
@@ -1,4 +1,7 @@
-
+SELECT id, movie, description, rating
+FROM Cinema
+WHERE id % 2 <> 0 AND description <> "boring"
+ORDER BY rating DESC; 
 
 -- Write a solution to find for each user, the join date and the number of orders they made as a buyer in 2019.
 -- Return the result table in any order.

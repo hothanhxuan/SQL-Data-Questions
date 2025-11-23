1c2608e2f484295fe6d430c5a8bcad0f147c980f
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-11-23 02:03:16 +0200

    Update ReformatDepartmentTable.sql

--- LeetCode/Easy/ReformatDepartmentTable.sql
+++ LeetCode/Easy/ReformatDepartmentTable.sql
@@ -1,3 +1,22 @@
+SELECT 
+    id, 
+    sum( if( month = 'Jan', revenue, null ) ) AS Jan_Revenue,
+    sum( if( month = 'Feb', revenue, null ) ) AS Feb_Revenue,
+    sum( if( month = 'Mar', revenue, null ) ) AS Mar_Revenue,
+    sum( if( month = 'Apr', revenue, null ) ) AS Apr_Revenue,
+    sum( if( month = 'May', revenue, null ) ) AS May_Revenue,
+    sum( if( month = 'Jun', revenue, null ) ) AS Jun_Revenue,
+    sum( if( month = 'Jul', revenue, null ) ) AS Jul_Revenue,
+    sum( if( month = 'Aug', revenue, null ) ) AS Aug_Revenue,
+    sum( if( month = 'Sep', revenue, null ) ) AS Sep_Revenue,
+    sum( if( month = 'Oct', revenue, null ) ) AS Oct_Revenue,
+    sum( if( month = 'Nov', revenue, null ) ) AS Nov_Revenue,
+    sum( if( month = 'Dec', revenue, null ) ) AS Dec_Revenue
+FROM 
+    Department
+GROUP BY 
+    id;
+
 -- Reformat the table such that there is a department id column and a revenue column for each month.
 -- Return the result table in any order.
 -- The result format is in the following example.

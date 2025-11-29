09d8582bca7f0b4242a390df16ef2bfd6cc2ab2e
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-11-29 22:43:42 +0200

    Update QueriesQualityandPercentage.sql

--- LeetCode/Easy/QueriesQualityandPercentage.sql
+++ LeetCode/Easy/QueriesQualityandPercentage.sql
@@ -1,3 +1,9 @@
+SELECT 
+    query_name
+    ,ROUND((SUM(rating / position)/COUNT(query_name)), 2) AS quality
+    ,ROUND((SUM(CASE WHEN rating < 3 then 1 else 0 end)/ COUNT(query_name)) * 100, 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
 
 -- We define query quality as:
 -- The average of the ratio between query rating and its position.

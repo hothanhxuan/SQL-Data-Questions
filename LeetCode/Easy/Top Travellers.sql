e240b651eed84f759b4598835c242bcfd352339f
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-12-20 00:51:33 +0200

    Update Top Travellers.sql

--- LeetCode/Easy/Top Travellers.sql
+++ LeetCode/Easy/Top Travellers.sql
@@ -1,3 +1,9 @@
+SELECT U.name, IFNULL(SUM(R.distance), 0) AS travelled_distance
+FROM Users AS U 
+LEFT JOIN Rides AS R 
+ON U.id = R.user_id 
+GROUP BY U.id
+ORDER BY travelled_distance DESC, U.name 
 
 -- Write a solution to report the distance traveled by each user.
 

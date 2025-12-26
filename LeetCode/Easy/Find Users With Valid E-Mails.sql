897da9cc269d2e151a44a5a9300f5c8d0e338f9d
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-12-26 19:06:33 +0200

    Update Find Users With Valid E-Mails.sql

--- LeetCode/Easy/Find Users With Valid E-Mails.sql
+++ LeetCode/Easy/Find Users With Valid E-Mails.sql
@@ -1,3 +1,7 @@
+SELECT *
+FROM Users 
+WHERE REGEXP_LIKE(mail, '^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode[.]com$', 'c');
+
 -- Write a solution to find the users who have valid emails.
 -- A valid e-mail has a prefix name and a domain where:
 

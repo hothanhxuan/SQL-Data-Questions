0dbd9cfeea217ec0fc1102381352f994501c18bd
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2025-12-28 00:29:33 +0700

    Update Group Sold Products By The Date.sql

--- LeetCode/Easy/Group Sold Products By The Date.sql
+++ LeetCode/Easy/Group Sold Products By The Date.sql
@@ -1,3 +1,9 @@
+SELECT sell_date
+        ,COUNT(DISTINCT product) AS num_sold
+        ,GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR  ',') AS products 
+FROM Activities 
+GROUP BY sell_date
+ORDER BY sell_date;
 
 -- Write a solution to find for each date the number of different products sold and their names.
 -- The sold products names for each date should be sorted lexicographically.

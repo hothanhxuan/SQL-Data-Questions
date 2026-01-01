af3ef087724c8694c6f7dc9b020f0422cb1c6fff
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-01-01 20:31:39 +0200

    Update Patients With a Condition.sql

--- LeetCode/Easy/Patients With a Condition.sql
+++ LeetCode/Easy/Patients With a Condition.sql
@@ -1,3 +1,6 @@
+SELECT *
+FROM Patients
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%';
 
 -- Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. 
 -- Type I Diabetes always starts with DIAB1 prefix.

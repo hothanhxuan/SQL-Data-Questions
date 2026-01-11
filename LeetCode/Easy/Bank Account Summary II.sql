8eb6d10f98e0b2ea4b476e5762963dafc25cede5
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-01-12 03:59:32 +0700

    Update Bank Account Summary II.sql

--- LeetCode/Easy/Bank Account Summary II.sql
+++ LeetCode/Easy/Bank Account Summary II.sql
@@ -1,4 +1,10 @@
-
+SELECT raw.name, SUM(raw.amount) as balance 
+FROM (SELECT U.account, U.name, T.amount
+        FROM Users as U
+        RIGHT JOIN Transactions as T 
+        ON U.account = T.account) as raw
+GROUP BY raw.name
+HAVING balance > 10000;
 -- Write a solution to report the name and balance of users with a balance higher than 10000. 
 -- The balance of an account is equal to the sum of the amounts of all transactions involving that account.
 -- Return the result table in any order.

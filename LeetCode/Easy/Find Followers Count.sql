277f49c7b02bb5ab2b088133f1dd514762a2a1ea
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-02-12 23:54:57 +0200

    Update Find Followers Count.sql

--- LeetCode/Easy/Find Followers Count.sql
+++ LeetCode/Easy/Find Followers Count.sql
@@ -1,27 +1,27 @@
 
-Write a solution that will, for each user, return the number of followers.
+-- Write a solution that will, for each user, return the number of followers.
 
-Return the result table ordered by user_id in ascending order.
+-- Return the result table ordered by user_id in ascending order.
 
-Input: 
-Followers table:
-+---------+-------------+
-| user_id | follower_id |
-+---------+-------------+
-| 0       | 1           |
-| 1       | 0           |
-| 2       | 0           |
-| 2       | 1           |
-+---------+-------------+
-Output: 
-+---------+----------------+
-| user_id | followers_count|
-+---------+----------------+
-| 0       | 1              |
-| 1       | 1              |
-| 2       | 2              |
-+---------+----------------+
-Explanation: 
-The followers of 0 are {1}
-The followers of 1 are {0}
-The followers of 2 are {0,1}
+-- Input: 
+-- Followers table:
+-- +---------+-------------+
+-- | user_id | follower_id |
+-- +---------+-------------+
+-- | 0       | 1           |
+-- | 1       | 0           |
+-- | 2       | 0           |
+-- | 2       | 1           |
+-- +---------+-------------+
+-- Output: 
+-- +---------+----------------+
+-- | user_id | followers_count|
+-- +---------+----------------+
+-- | 0       | 1              |
+-- | 1       | 1              |
+-- | 2       | 2              |
+-- +---------+----------------+
+-- Explanation: 
+-- The followers of 0 are {1}
+-- The followers of 1 are {0}
+-- The followers of 2 are {0,1}

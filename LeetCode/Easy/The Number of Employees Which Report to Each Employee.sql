8299eb2224ac1562fb58c95f4df377f7c1d92187
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-02-22 20:10:45 +0200

    Update The Number of Employees Which Report to Each Employee.sql

--- LeetCode/Easy/The Number of Employees Which Report to Each Employee.sql
+++ LeetCode/Easy/The Number of Employees Which Report to Each Employee.sql
@@ -1,3 +1,14 @@
+SELECT 
+    e.employee_id,
+    e.name,
+    COUNT(r.employee_id) AS reports_count,
+    ROUND(AVG(r.age)) AS average_age
+FROM Employees e
+JOIN Employees r
+    ON e.employee_id = r.reports_to
+GROUP BY e.employee_id, e.name
+ORDER BY e.employee_id;
+
 -- We will consider a manager an employee who has at least 1 other employee reporting to them.
 
 -- Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.

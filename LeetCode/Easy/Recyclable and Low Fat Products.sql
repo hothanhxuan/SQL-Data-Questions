078b7328ef0bb37e2d28fd2cbde23e56758415e3
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-03-04 00:36:05 +0200

    Update Recyclable and Low Fat Products.sql

--- LeetCode/Easy/Recyclable and Low Fat Products.sql
+++ LeetCode/Easy/Recyclable and Low Fat Products.sql
@@ -1,3 +1,8 @@
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y'
+  AND recyclable = 'Y';
+
 -- Write a solution to find the ids of products that are both low fat and recyclable.
 -- Return the result table in any order.
 

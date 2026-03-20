4d52490ac5957e4e78e1357e675b1cf8c48892da
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-03-20 23:26:21 +0200

    Update The Latest Login in 2020.sql

--- LeetCode/Easy/The Latest Login in 2020.sql
+++ LeetCode/Easy/The Latest Login in 2020.sql
@@ -1,12 +1,5 @@
 -- Write a solution to report the latest login for all users in the year 2020. Do not include the users who did not login in 2020.
-
 -- Return the result table in any order.
-
--- The result format is in the following example.
-
- 
-
--- Example 1:
 
 -- Input: 
 -- Logins table:

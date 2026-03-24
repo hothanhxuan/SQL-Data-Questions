f337197f4309536caa8049c963afaf2d7bbb1249
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-03-25 00:00:27 +0200

    Update Employees With Missing Information.sql

--- LeetCode/Easy/Employees With Missing Information.sql
+++ LeetCode/Easy/Employees With Missing Information.sql
@@ -1,40 +1,40 @@
-Write a solution to report the IDs of all the employees with missing information. The information of an employee is missing if:
+-- Write a solution to report the IDs of all the employees with missing information. The information of an employee is missing if:
 
-The employee's name is missing, or
-The employee's salary is missing.
-Return the result table ordered by employee_id in ascending order.
+-- The employee's name is missing, or
+-- The employee's salary is missing.
+-- Return the result table ordered by employee_id in ascending order.
 
-The result format is in the following example.
+-- The result format is in the following example.
 
  
 
-Example 1:
+-- Example 1:
 
-Input: 
-Employees table:
-+-------------+----------+
-| employee_id | name     |
-+-------------+----------+
-| 2           | Crew     |
-| 4           | Haven    |
-| 5           | Kristian |
-+-------------+----------+
-Salaries table:
-+-------------+--------+
-| employee_id | salary |
-+-------------+--------+
-| 5           | 76071  |
-| 1           | 22517  |
-| 4           | 63539  |
-+-------------+--------+
-Output: 
-+-------------+
-| employee_id |
-+-------------+
-| 1           |
-| 2           |
-+-------------+
-Explanation: 
-Employees 1, 2, 4, and 5 are working at this company.
-The name of employee 1 is missing.
-The salary of employee 2 is missing.
+-- Input: 
+-- Employees table:
+-- +-------------+----------+
+-- | employee_id | name     |
+-- +-------------+----------+
+-- | 2           | Crew     |
+-- | 4           | Haven    |
+-- | 5           | Kristian |
+-- +-------------+----------+
+-- Salaries table:
+-- +-------------+--------+
+-- | employee_id | salary |
+-- +-------------+--------+
+-- | 5           | 76071  |
+-- | 1           | 22517  |
+-- | 4           | 63539  |
+-- +-------------+--------+
+-- Output: 
+-- +-------------+
+-- | employee_id |
+-- +-------------+
+-- | 1           |
+-- | 2           |
+-- +-------------+
+-- Explanation: 
+-- Employees 1, 2, 4, and 5 are working at this company.
+-- The name of employee 1 is missing.
+-- The salary of employee 2 is missing.

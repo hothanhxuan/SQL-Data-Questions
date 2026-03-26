fb7abda5a1303d003ee320f1b67244b7aef9b270
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-03-27 00:34:12 +0200

    Update Employees With Missing Information.sql

--- LeetCode/Easy/Employees With Missing Information.sql
+++ LeetCode/Easy/Employees With Missing Information.sql
@@ -1,3 +1,19 @@
+SELECT e.employee_id
+FROM Employees e
+LEFT JOIN Salaries s 
+    ON e.employee_id = s.employee_id
+WHERE e.name IS NULL OR s.salary IS NULL
+
+UNION
+
+SELECT s.employee_id
+FROM Salaries s
+LEFT JOIN Employees e 
+    ON s.employee_id = e.employee_id
+WHERE e.name IS NULL OR s.salary IS NULL
+
+ORDER BY employee_id;
+
 -- Write a solution to report the IDs of all the employees with missing information. The information of an employee is missing if:
 
 -- The employee's name is missing, or

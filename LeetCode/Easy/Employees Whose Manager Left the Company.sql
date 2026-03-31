56af0abb01027f2369452434b0264eae0b90bfe8
Susan Ho <164034461+hothanhxuan@users.noreply.github.com>
2026-04-01 00:05:12 +0300

    Update Employees Whose Manager Left the Company.sql

--- LeetCode/Easy/Employees Whose Manager Left the Company.sql
+++ LeetCode/Easy/Employees Whose Manager Left the Company.sql
@@ -1,3 +1,12 @@
+SELECT e.employee_id
+FROM Employees e
+LEFT JOIN Employees m
+    ON e.manager_id = m.employee_id
+WHERE e.salary < 30000
+  AND e.manager_id IS NOT NULL
+  AND m.employee_id IS NULL
+ORDER BY e.employee_id;
+
 -- Find the IDs of the employees whose salary is strictly less than $30000 and whose manager left the company. 
 -- When a manager leaves the company, their information is deleted from the Employees table, 
 -- but the reports still have their manager_id set to the manager that left.
